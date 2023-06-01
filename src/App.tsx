67e78d260c414feb57567c562808a67c36e8e79e
Dinujaya Sandaruwan <info.dinujaya@gmail.com>
2023-06-01 15:07:54 +0530

    Added Background

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import Background from './components/Background';
 import Preview from './components/Preview';
 
 function App() {
     return (
         <div className="App">
-            <Preview />
+            <Background />
+            {/* <Preview /> */}
         </div>
     );
 }
---
+++ src/components/Background.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Background: React.FC = () => {
+    return (
+        <div className="container">
+            <div className="sky">
+                <div className="stars"></div>
+                <div className="stars1"></div>
+                <div className="stars2"></div>
+                <div className="shooting-stars"></div>
+            </div>
+        </div>
+    );
+};
+
+export default Background;
